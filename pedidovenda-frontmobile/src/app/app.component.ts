ea1d87bd11aa76d4baadde0469feec782c175348
Rodrigo Baggio <rodrigo.baggio.si@gmail.com>
2021-09-24 00:23:29 -0300

    Acesso ao carrinho: botão flutuante e menu

--- pedidovenda-frontmobile/src/app/app.component.ts
+++ pedidovenda-frontmobile/src/app/app.component.ts
@@ -24,6 +24,7 @@ export class MyApp {
 
     this.pages = [
       { title: "Categorias", component: "CategoriasPage" },
+      { title: "Carrinho", component: "CartPage" },
       { title: "Perfil", component: "ProfilePage" },
       { title: "Sair", component: "" },
     ];
