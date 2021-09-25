38dd88fb6b9887c19f5fbf40d7678f65b254dea6
Rodrigo Baggio <rodrigo.baggio.si@gmail.com>
2021-09-25 18:09:08 -0300

    Salvando pedido

---
+++ pedidovenda-frontmobile/src/services/domain/pedido.service.ts
@@ -0,0 +1,16 @@
+import { API_CONFIG } from "./../../config/api.config";
+import { PedidoDto } from "./../../models/pedido.dto";
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+
+@Injectable()
+export class PedidoService {
+  constructor(public http: HttpClient) {}
+
+  insert(obj: PedidoDto) {
+    return this.http.post(`${API_CONFIG.baseUrl}/pedidos`, obj, {
+      observe: "response",
+      responseType: "text",
+    });
+  }
+}
