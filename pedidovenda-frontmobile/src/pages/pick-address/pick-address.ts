50defd22fab71fad4d69d733de92c4da4e96a38a
Rodrigo Baggio <rodrigo.baggio.si@gmail.com>
2021-09-25 15:57:55 -0300

    Buscando endereços do banco de dados

--- pedidovenda-frontmobile/src/pages/pick-address/pick-address.ts
+++ pedidovenda-frontmobile/src/pages/pick-address/pick-address.ts
@@ -1,5 +1,7 @@
 import { Component } from "@angular/core";
 import { IonicPage, NavController, NavParams } from "ionic-angular";
+import { ClienteService } from "../../services/domain/cliente.service";
+import { StorageService } from "../../services/storage.service";
 
 import { EnderecoDTO } from "./../../models/endereco.dto";
 
@@ -11,42 +13,28 @@ import { EnderecoDTO } from "./../../models/endereco.dto";
 export class PickAddressPage {
   enderecos: EnderecoDTO[];
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {}
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    public storageService: StorageService,
+    public clienteService: ClienteService
+  ) {}
 
   ionViewDidLoad() {
-    this.enderecos = [
-      {
-        id: "1",
-        logradouro: "Rua Quinze de Novembro",
-        numero: "300",
-        complemento: "Apto 200",
-        bairro: "Santa Mônica",
-        cep: "48293822",
-        cidade: {
-          id: "1",
-          nome: "Uberlândia",
-          estado: {
-            id: "1",
-            nome: "Minas Gerais",
-          },
+    let localUser = this.storageService.getLocalUser();
+    if (localUser && localUser.email) {
+      this.clienteService.findByEmail(localUser.email).subscribe(
+        (response) => {
+          this.enderecos = response["enderecos"];
         },
-      },
-      {
-        id: "2",
-        logradouro: "Rua Alexandre Toledo da Silva",
-        numero: "405",
-        complemento: null,
-        bairro: "Centro",
-        cep: "88933822",
-        cidade: {
-          id: "3",
-          nome: "São Paulo",
-          estado: {
-            id: "2",
-            nome: "São Paulo",
-          },
-        },
-      },
-    ];
+        (error) => {
+          if (error.status) {
+            this.navCtrl.setRoot("HomePage");
+          }
+        }
+      );
+    } else {
+      this.navCtrl.setRoot("HomePage");
+    }
   }
 }
