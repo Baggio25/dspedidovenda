c89d6b365436a496c1d1984fcd10777104c3e9c2
Rodrigo Baggio <rodrigo.baggio.si@gmail.com>
2021-09-25 21:46:01 -0300

    Componente Loader

--- pedidovenda-frontmobile/src/pages/produtos/produtos.ts
+++ pedidovenda-frontmobile/src/pages/produtos/produtos.ts
@@ -1,5 +1,10 @@
 import { Component } from "@angular/core";
-import { IonicPage, NavController, NavParams } from "ionic-angular";
+import {
+  IonicPage,
+  LoadingController,
+  NavController,
+  NavParams,
+} from "ionic-angular";
 import { API_CONFIG } from "../../config/api.config";
 import { ProdutoDTO } from "../../models/produto.dto";
 import { ProdutoService } from "../../services/domain/produto.service";
@@ -15,15 +20,18 @@ export class ProdutosPage {
   constructor(
     public navCtrl: NavController,
     public navParams: NavParams,
-    public produtoService: ProdutoService
+    public produtoService: ProdutoService,
+    public loadingCtrl: LoadingController
   ) {}
 
   ionViewDidLoad() {
     let categoria_id = this.navParams.get("categoria_id");
+    let loader = this.presentLoading();
     this.produtoService.findByCategoria(categoria_id).subscribe(
       (response) => {
         this.items = response["content"];
         this.loadImageUrls();
+        loader.dismiss();
       },
       (error) => {}
     );
@@ -45,5 +53,13 @@ export class ProdutosPage {
 
   showDetail(produto_id: string) {
     this.navCtrl.push("ProdutoDetailPage", { produto_id: produto_id });
+  }
+
+  presentLoading() {
+    let loader = this.loadingCtrl.create({
+      content: "Carregando...",
+    });
+    loader.present();
+    return loader;
   }
 }
